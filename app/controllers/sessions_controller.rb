40ab099e9e1fb8567a6328c184cfe0b41d8eb88d
Vincent Oe <vincent.satoru.oe@gmail.com>
2013-06-24 01:02:17 -0700

    got basic sessions created. Working on proper renders for home page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,10 @@
 class SessionsController < ApplicationController
 
 	def new
+		unless !signed_in?
+       		flash[:notice] = "Already signed in"
+       		redirect_to root_path
+     	end
 	end
 
 	def create
