3ffd2070812a80cc95e65587c7a6ffb88f3768e8
Vincent Oe <vincent.satoru.oe@gmail.com>
2013-06-25 01:19:31 -0700

    user updating, friendly redirection, basic ind for users done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
+
+  before_filter :signed_in_user,  only: [:edit, :update]
+  before_filter :correct_user,    only: [:edit, :update]
   
-  def index
+  def index #needs to be for a specific group!!!!!!
     @users = User.all
   end
 
@@ -19,14 +22,42 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-       sign_in @user
-       flash[:success] = "Welcome to the Sample App!"
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end 
   end
 
+  def edit
+  end
 
+  def update
+    if @user.update_attributes(params[:user])
+      sign_in @user
+      flash[:success] = "Update Successful!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+private
+
+def signed_in_user
+  unless signed_in?
+    store_location
+    redirect_to signin_url, notice: "Please sign in"
+  end
 end
 
+def correct_user
+  @user = User.find(params[:id])
+  unless current_user?(@user)
+    store_location
+    redirect_to users_path, notice: 'Cannot access another users information'
+  end
+end
+
+end
