fe825388bf249808c3955d6536f66b40dec1ca51
Vincent Oe <vincent.satoru.oe@gmail.com>
2013-06-24 23:26:04 -0700

    sessions created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -21,7 +25,8 @@ class UsersController < ApplicationController
     else
       render 'new'
     end 
-   end
+  end
+
 
 end
 
